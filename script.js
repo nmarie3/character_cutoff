86aeda57964a48d1a22350297714aae3b01aa329
Natasha Martincek <natasha.marie33@gmail.com>
2023-03-31 15:29:24 +0900

    added slider

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const resultBox =document.querySelector('#result-box');
-const limitLine =document.querySelector('#limit-line')
+const textInput = document.querySelector('#text-input');
+const resultBox = document.querySelector('#result-box');
+const limitRedLine = document.querySelector('#limit-line');
+let limitSlider = document.querySelector('#limit-slider');
 
-document.querySelector('#text-input').addEventListener("input", updateText);
-document.querySelector('#size-input').addEventListener("input", updateFont);
-document. querySelector('#font-select').addEventListener("change", updateFont);
+textInput.addEventListener("input", updateText);
+limitSlider.addEventListener('input', updateLimitLine);
+// document.querySelector('#size-input').addEventListener("input", updateFont);
+// document. querySelector('#font-select').addEventListener("change", updateFont);
 
 
 function updateText() {
-    const text =document.querySelector('#text-input').value;
+    const text = document.querySelector('#text-input').value;
     resultBox.innerText = text;
 }
 
@@ -16,4 +19,10 @@ function updateFont() {
     const size = document.querySelector('#size-input').value + "px";
     resultBox.style.fontFamily = font;
     resultBox.style.fontSize = size;
+}
+
+function updateLimitLine () {
+    const sliderValue = limitSlider.value;
+    const limitLine = limitRedLine;
+    limitLine.style.left =sliderValue + '%';
 }
